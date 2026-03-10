3d50a70aa93650426b7e98ead5dca35dc104339b
kiran-kharsel <kirankharsel@gmail.com>
2026-03-10 09:17:51 +0530

    feat: create traffic-light data

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import TrafficLight from './traffic-light'
 
+const TrafficLights = [
+  {
+    id: 1,
+    color: 'red',
+    time: 1500, //in ms
+    lightingOrder: 1,
+    positionOrder: 1,
+  },
+  {
+    id: 2,
+    color: 'yellow',
+    time: 1500, //in ms
+    lightingOrder: 2,
+    positionOrder: 2,
+  },
+  {
+    id: 3,
+    color: 'green',
+    time: 2000, //in ms
+    lightingOrder: 3,
+    positionOrder: 3,
+  },
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <TrafficLight/>
+      <TrafficLight data={TrafficLights}/>
     </>
   )
 }
